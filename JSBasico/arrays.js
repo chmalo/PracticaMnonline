69665f01f18e8f96fbc730a6155fc18d9dddf728
Chmalo <chmalo.f@gmail.com>
2020-03-17 10:08:10 -0400

    JSbasico v4

--- JSBasico/arrays.js
+++ JSBasico/arrays.js
@@ -60,3 +60,45 @@ function Auto(marca, modelo, annio) {
 var autoNuevo = new Auto("Ford", "Mustang", 2020);
 var autoNuevo2 = new Auto("Tesla", "Model x", 2019);
 var autoNuevo3 = new Auto("Toyota", "Corolla", 2020);
+
+
+
+//recorrido de arrays
+
+var articulos = [
+    {nombre: "Bici", costo: 3000},
+    {nombre: "Tv", costo: 2500},
+    {nombre: "Libro", costo: 320},
+    {nombre: "Celular", costo: 10000},
+    {nombre: "Laptop", costo: 20000},
+    {nombre: "Teclado", costo: 500},
+    {nombre: "Audifonos", costo: 1700},
+];
+
+//filtro de array 
+var articulosFiltrados = articulos.filter(function(articulo) {
+    return articulo.costo <= 500    
+});
+
+//metodo map para mapear los arrays
+var nombreArticulos = articulos.map(function(articulo) {
+    return articulo.nombre 
+});
+
+//metodo find para buscar en arrays en especifico
+
+var encuentraArticulo = articulos.find(function(articulo) {
+    return articulo.nombre === "Laptop";    
+});
+
+
+//metodo forEach
+articulos.forEach(function (articulo) {
+    console.log(articulo.nombre)    
+});
+
+//metodo some valida verdadero o falso
+
+var articulosBaratos = articulos.some(function(articulo) {
+    return articulo.costo <= 700;    
+})
