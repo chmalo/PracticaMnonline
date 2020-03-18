875f5cb7e6a5fceffb3f189f2becf73b3abfe04b
Chmalo <chmalo.f@gmail.com>
2020-03-18 12:02:18 -0400

    VueBasico v3

--- VueBasico/app.js
+++ VueBasico/app.js
@@ -1,56 +1,122 @@
-new Vue({
-    el: "#app",
-    data: {
-        name: 'Bitcoin',
-        symbol: 'BTC',
-        img: 'https://bitcoin.org/img/icons/opengraph.png?1583780769',
-        changePercent: 10,
+Vue.component('CoinDetail', {
+    props: ['coin'],
 
-        value: 0,
-
-        color: '#f4f4f4',
-
-        price: 8400,
-
-        pricesWithDays:[
-            {day: 'Lunes', valor: 8400},
-            {day: 'Martes', valor: 7900},
-            {day: 'Miercoles', valor: 8200},
-            {day: 'Jueves', valor: 9000},
-            {day: 'Viernes', valor: 9400},
-            {day: 'Sabado', valor: 10000},
-            {day: 'Domingo', valor: 10200}
-        ],
-
-        showPrices: false
-    },
-
-    computed: {
-        title (){
-            return `${this.name} - ${this.symbol}`;
-        },
-
-        convertedValue (){
-            if(!this.value){
-                return 0;
-            }
-
-            return this.value /this.price;
+    data(){
+        return {
+            showPrices: false,
+            value: 0,
         }
     },
 
-    watch: {
-        showPrices(newVal, oldVal){
-            console.log(newVal, oldVal)
-        }
+
+    created() {
+        console.log('created de CoinDetail...')
+    },
+
+
+    mounted() {
+        console.log('mounted de CoinDetail...')
     },
 
     methods: {
-        toggleShowPrices(){
+        toggleShowPrices() {
             this.showPrices = !this.showPrices;
 
-            this.color = this.color.split('')
-            .reverse().join('')
+            this.$emit('change-color', 
+                this.showPrices ? 'FF96C8' : '3D3D3D')
+        }
+    },
+
+    computed: {
+        title() {
+            return `${this.coin.name} - ${this.coin.symbol}`;
+        },
+
+        convertedValue() {
+            if (!this.value) {
+                return 0;
+            }
+
+            return this.value / this.coin.price;
+        }
+    },
+    
+    template: `
+    <div>
+        <img 
+            v-on:mouseover="toggleShowPrices" 
+            v-on:mouseOut="toggleShowPrices"
+            v-bind:src="coin.img" v-bind:alt="coin.name">
+
+        <h1 
+            v-bind:class="coin.changePercent > 0 ? 'green' : 'red'">
+            {{ title }}
+            <span v-if="coin.changePercent > 0"> :) </span>
+            <span v-else-if="coin.changePercent < 0"> :( </span>
+            <span v-else> :| </span>
+
+            <span v-on:click="toggleShowPrices">
+                {{ showPrices? 'Precios': 'Ver precios' }}
+            </span>
+        </h1>
+
+        <input type="number" v-model="value">
+        <span>{{ convertedValue }}</span>
+
+        <slot name="text"></slot>
+        <slot name="link"></slot>
+
+        <ul v-show="showPrices">
+            <li 
+            class="uppercase"
+            v-bind:class="{ orange: p.valor == coin.price, red: p.valor < coin.price, green: p.valor > coin.price }"
+            v-for="(p, i) in coin.pricesWithDays" 
+            v-bind:key="p.day">
+                {{ i }} - {{ p.day }} - {{ p.valor }}
+            </li>
+        </ul>
+    </div>
+    `   
+})
+
+new Vue({
+    el: "#app",
+    data: {
+        btc: {
+            name: 'Bitcoin',
+            symbol: 'BTC',
+            img: 'https://bitcoin.org/img/icons/opengraph.png?1583780769',
+            changePercent: 10,
+            price: 8400,
+            pricesWithDays: [
+                { day: 'Lunes', valor: 8400 },
+                { day: 'Martes', valor: 7900 },
+                { day: 'Miercoles', valor: 8200 },
+                { day: 'Jueves', valor: 9000 },
+                { day: 'Viernes', valor: 9400 },
+                { day: 'Sabado', valor: 10000 },
+                { day: 'Domingo', valor: 10200 }
+            ],
+        },
+
+        color: '#f4f4f4',
+    },
+
+    created() {
+        console.log('created...')
+    },
+    
+
+    mounted() {
+        console.log('mounted...')
+    },
+
+    methods: {
+        updateColor(color){
+           this.color = color || this.color
+           .split('')
+           .reverse()
+           .join('')
         }
     }
 });
